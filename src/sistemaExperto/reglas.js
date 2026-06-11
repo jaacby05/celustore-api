66a4cf2fe147f32c8575723b49cc1013d1ec3c7c
jaacby05 <jaacby05@gmail.com>
2026-06-11 03:19:26 -0300

    Update reglas.js

--- src/sistemaExperto/reglas.js
+++ src/sistemaExperto/reglas.js
@@ -1,1 +1,20 @@
+// src/sistemaExperto/reglas.js
 
+// Definimos los perfiles lógicos basados en las necesidades del cliente
+const reglasPerfiles = {
+    gaming: {
+        ramMinima: 8,
+        almacenamientoMinimo: 128,
+        procesadorRequerido: ['Snapdragon', 'Dimensity', 'Apple A']
+    },
+    fotografia: {
+        camaraMinimaMP: 48,
+        ramMinima: 6
+    },
+    basico: {
+        ramMinima: 4,
+        bateriaMinimaMAh: 4000
+    }
+};
+
+module.exports = { reglasPerfiles };
